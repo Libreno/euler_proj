e35190e9edce4040c59969ebb9578c98033d4976
Farkhad Muminov <libreno@gmail.com>
2020-08-14 18:57:55 +0400

    added 54, 59 solutions

---
+++ 59/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace FunctionalStuff
+{
+	class Program
+		static void Main(string[] args)
+		{
+			var sw = new Stopwatch();
+			sw.Start();
+			Console.WriteLine(Problem59.Solve("cipher1.txt"));
+			sw.Stop();
+			Console.WriteLine("Time: " + sw.ElapsedMilliseconds);
+			sw.Reset();
+		}
+	}
+}
